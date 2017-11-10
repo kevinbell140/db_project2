e3bf5599354a7a9812410c77bd47408d8141becc
Kevin Bell <kbell140@gmail.com>
2017-11-10 11:38:59 -0500

    Add files via upload

---
+++ insertCertification.sql
@@ -0,0 +1,10 @@
+Insert CERTIFICATION (CERTIFICATION_NAME, CERTIFICATION_DESC) 
+Values 
+	('G', 'For all audiences'), 
+	('PG', 'Parental Guidance Suggested (mainly for under 10s)'), 
+	('PG-13', 'Parental Guidance Suggested for children under 13'), 
+	('R', 'Under 17 not admitted without parent or guardian'),
+	('NC-17', 'Under 17 not admitted'),
+	('Approved',  'Pre-1968 titles only (from the MPAA site) Under the Hays Code, films were simply approved or disapproved based on whether they were deemed "moral" or "immoral".)');
+
+
