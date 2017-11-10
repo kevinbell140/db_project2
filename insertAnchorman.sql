a3de1b4c80627c4f6534f4acadb73067cba4e317
Kevin Bell <kbell140@gmail.com>
2017-11-10 12:27:52 -0500

    Add files via upload

---
+++ insertAnchorman.sql
@@ -0,0 +1,6 @@
+insert FILM (FILM_NAME, FILM_DESC, FILM_RELEASE_DATE, CERTIFICATION_ID,FILM_GENRE_ID)
+values ('Anchorman: The Legend of Ron Burgundy', 
+		'Ron Burgundy is San Diego`s top-rated newsman in the male-dominated broadcasting of the 1970s, but that`s all about to change for Ron and his cronies when an ambitious woman is hired as a new anchor.',
+		'20040709',
+		15,
+		6);
